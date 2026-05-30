3e9a73a4c6025ab0fda626bfa3fda34e8c8fcf8c
mfranmartins <mfran.martiins@gmail.com>
2026-05-29 21:10:34 -0300

    Atualização no banco de dados

--- confeitaria.sql
+++ confeitaria.sql
@@ -1,10 +1,15 @@
-CREATE DATABASE IF NOT EXISTS `confeitaria_tcc`;
+CREATE DATABASE  IF NOT EXISTS `confeitaria_tcc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `confeitaria_tcc`;
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: confeitaria_tcc
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -13,10 +18,41 @@ USE `confeitaria_tcc`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Estrutura da tabela `usuarios`
+-- Table structure for table `itens_cardapio`
+--
+
+DROP TABLE IF EXISTS `itens_cardapio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itens_cardapio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  `imagem` varchar(255) NOT NULL DEFAULT 'padrao.png',
+  `categoria` varchar(100) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'ativo',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itens_cardapio`
+--
+
+LOCK TABLES `itens_cardapio` WRITE;
+/*!40000 ALTER TABLE `itens_cardapio` DISABLE KEYS */;
+INSERT INTO `itens_cardapio` VALUES (1,'Ovo trufado','sdasdsadsa',50.00,'6a0abc73238bf.png','Ovos de Páscoa','ativo'),(3,'dasdsadsa','asdasdsad',9.98,'6a17b9f391271.jpeg','asdsad','ativo'),(4,'asdasdsadasdas','ssdssssdsaaa',60.00,'6a17ba0aca6a7.jpg','asdsad','ativo');
+/*!40000 ALTER TABLE `itens_cardapio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
 --
 
 DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
@@ -32,38 +68,20 @@ CREATE TABLE `usuarios` (
   `tipo` enum('cliente','admin') DEFAULT 'cliente',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dados da tabela `usuarios`
+-- Dumping data for table `usuarios`
 --
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
--- ID 1: Admin Mestre com o hash correto da senha 'admin123'
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `telefone`, `senha`, `data_nascimento`, `cpf`, `rua`, `numero`, `bairro`, `cidade`, `tipo`) VALUES 
-(1,'Admin Mestre','admin@confeitaria.com',NULL,'$2y$10$vI876bWnU.TjG0rF.5YJ/.3k9n7eR6yG6a6.S2Y9W.vK3G/D6e2eO',NULL,NULL,NULL,NULL,NULL,NULL,'admin'),
-(2,'Marcelo Martins','mfran.martiins@gmail.com','(11) 91235-3101','$2y$10$Y0gCbkFgcDv1zIlhHlgRJuaLHedBGfu4Q8bmRwBM2GlW/FJ2eDpoG','2007-07-02','428.681.868-31','Rua Constância Asson','394','Jardim Três Marias','São Paulo','cliente');
+INSERT INTO `usuarios` VALUES (2,'Marcelo Martins','mfran.martiins@gmail.com','(11) 91235-3101','$2y$10$Y0gCbkFgcDv1zIlhHlgRJuaLHedBGfu4Q8bmRwBM2GlW/FJ2eDpoG','2007-07-02','428.681.868-31','Rua Constância Asson','394','Jardim Três Marias','São Paulo','cliente'),(3,'Admin Mestre','admin@confeitaria.com',NULL,'$2y$10$MXaD7T0X8/qIMhxcWMcfFe.6F4mzmSh1NvNjEmy0D4RTqHZ67yj1W',NULL,NULL,NULL,NULL,NULL,NULL,'admin'),(4,'Marcelo Martins','marcelofranciscomartins07@gmail.com','(11) 91235-3101','$2y$10$MQ4K4hPC2ShXScbDhCz4rOJnKXhy4LAjCbPNGgnxwTzd/fctY4gZ2','2007-07-02','428.681.868-31','Rua Constância Asson','394','Jardim Três Marias','São Paulo','cliente'),(5,'Marcelo Martins','teste@gmail.com','11912353101','$2y$10$xhByJ7eqNGDcUf6JpN89N.pNp.an6DXpRq3O9dmcEi8Hpjj2CmMSy','2007-07-02','12345678911','Rua Constância Asson','445','Jardim Três Marias','São Paulo','cliente');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Estrutura da tabela `itens_cardapio`
---
-
-DROP TABLE IF EXISTS `itens_cardapio`;
-CREATE TABLE `itens_cardapio` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
-  `descricao` text NOT NULL,
-  `preco` decimal(10,2) NOT NULL,
-  `imagem` varchar(255) NOT NULL DEFAULT 'padrao.png',
-  `categoria` varchar(100) NOT NULL,
-  `status` varchar(20) NOT NULL DEFAULT 'ativo',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -71,3 +89,5 @@ CREATE TABLE `itens_cardapio` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-29 21:07:55
